3f84ccfe227671cdf323eb1a9e4b3ffaad0c2e82
Kyrylo <fresh.js.ftd.@gmail.com>
2024-11-11 20:47:10 +0200

    Add setTimeout and setInterval

--- assets/js/async.js
+++ assets/js/async.js
@@ -21,4 +21,41 @@ function sum() {
   return 1 + 1;
 }
 
-console.log(sum());
+// console.log(sum());
+
+// setTimeout - функція встановлення зворотнього відліку
+// setTimeout(function callback() {
+//   console.log('delayed');
+// }, 1000);
+
+
+console.log(1);
+
+console.log(2);
+
+const id = setTimeout(() => {
+  console.log(3);
+}, 0);
+
+console.log(4);
+// 1 2 4 3
+
+// відміна таймаута по його id
+clearTimeout(id);
+
+const id2 = setTimeout(() => {
+  console.log(5000);
+}, 5000);
+
+setTimeout(() => {
+  // clearTimeout(id2);
+  console.log(1000);
+}, 1000);
+
+// setInterval - встановлює інтервал, який буде працювати до зупинки запускаючи коллбек
+const intervalId = setInterval(() => {
+  console.log('interval');
+}, 1500);
+
+// зупинка інтервалу
+clearInterval(intervalId);
